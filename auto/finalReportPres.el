b4eca7de71c8d12d229f5a0f0e09671801dbbbe6
Md Suruz Miah <smiah@fsmail.bradley.edu>
2022-05-05 13:59:18 -0500

    ~

--- auto/finalReportPres.el
+++ auto/finalReportPres.el
@@ -4,13 +4,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("caption" "font=tiny" "labelfont=bf")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -19,7 +19,6 @@
     "pgfgantt"
     "todonotes"
     "media9"
-    "subfigure"
     "booktabs"
     "array"
     "caption"
@@ -27,11 +26,27 @@
     "graphicx"
     "siunitx"
     "arydshln"
-    "rotating")
+    "rotating"
+    "amsmath"
+    "amssymb"
+    "amsfonts"
+    "bm")
+   (LaTeX-add-labels
+    "fig:fleet"
+    "fig:steerSystem"
+    "fig:nnActor"
+    "eq:approximatePolicyActorWeights"
+    "eq:actorWeightsGradient"
+    "eq:actorWeightUpdate"
+    "fig:nonlinGraph"
+    "fig:vehicleSetup"
+    "fig:validationSetup"
+    "fig:byWireSteerModel"
+    "fig:manualSteerModel"
+    "fig:byWireAccelModel"
+    "fig:manualAccelModel")
    (LaTeX-add-environments
-    '("remark" LaTeX-env-args ["argument"] 1)
-    '("prelab" LaTeX-env-args ["argument"] 1)
-    '("example" LaTeX-env-args ["argument"] 1))
+    '("prelab" LaTeX-env-args ["argument"] 1))
    (LaTeX-add-bibliographies
     "bib/references.bib"))
  :latex)
